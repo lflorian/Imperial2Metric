9726ed96cb6ab2c7ab6a92dece3a05306de42682
Florian <florian.lammert@icloud.com>
2024-10-19 13:21:58 +0200

    Initial Commit

---
+++ Imperial2Metric/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Imperial2Metric
+//
+//  Created by Florian Lammert on 19.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Imperial2Metric/Imperial2MetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imperial2MetricApp.swift
+//  Imperial2Metric
+//
+//  Created by Florian Lammert on 19.10.24.
+//
+
+import SwiftUI
+
+@main
+struct Imperial2MetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
